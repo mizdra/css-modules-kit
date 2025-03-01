f24f4a0b0df700ff5c145f924e0781e31e04fc1c
mizdra <pp.mizdra@gmail.com>
2025-03-01 14:23:36 +0900

    chore: those options is actually used

--- packages/core/src/parser/location.ts
+++ packages/core/src/parser/location.ts
@@ -7,13 +7,11 @@ export interface Position {
    * The line number in the source file. It is 1-based.
    * This is compatible with postcss and tsserver.
    */
-  // TODO: Maybe it should be deleted since it is not used
   line: number;
   /**
    * The column number in the source file. It is 1-based.
    * This is compatible with postcss and tsserver.
    */
-  // TODO: Maybe it should be deleted since it is not used
   column: number;
   /** The offset in the source file. It is 0-based. */
   offset: number;
@@ -29,7 +27,6 @@ export interface Location {
    * The ending position of the node. It is exclusive.
    * This is compatible with tsserver, but not postcss.
    */
-  // TODO: Maybe it should be deleted since it is not used
   end: Position;
 }
 
