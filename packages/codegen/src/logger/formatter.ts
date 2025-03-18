5069985e6f7044a8ff671d4db7f7eb3a3a0802f8
mizdra <pp.mizdra@gmail.com>
2025-03-19 00:23:05 +0900

    feat!: drop support for `NODE_DISABLE_COLORS` and `FORCE_COLOR`

--- packages/codegen/src/logger/formatter.ts
+++ packages/codegen/src/logger/formatter.ts
@@ -6,57 +6,57 @@ import {
   relative,
   type SystemError,
 } from '@css-modules-kit/core';
+import { shouldBePretty } from './typescript.js';
 
-function shouldColorize(): boolean {
-  return (
-    !!process.env['FORCE_COLOR'] ||
-    (!process.env['NO_COLORS'] && !process.env['NODE_DISABLE_COLORS'] && process.stderr.hasColors())
-  );
+function color(color: Parameters<typeof styleText>[0], text: string, pretty: boolean): string {
+  return pretty ? styleText(color, text) : text;
 }
 
 export function formatDiagnostic(diagnostic: Diagnostic, cwd: string): string {
+  const pretty = shouldBePretty(undefined);
   let result = '';
   if ('file' in diagnostic) {
-    result += `${formatLocation(diagnostic.file.fileName, diagnostic.start, cwd)} - `;
+    result += `${formatLocation(diagnostic.file.fileName, diagnostic.start, cwd, pretty)} - `;
   }
-  result += `${formatCategory(diagnostic.category)}: `;
+  result += `${formatCategory(diagnostic.category, pretty)}: `;
   result += diagnostic.text;
   // TODO(#124): Add source code if diagnostics has a location
   return result;
 }
 
 export function formatSystemError(error: SystemError): string {
+  const pretty = shouldBePretty(undefined);
   let result = '';
-  result += `${formatCategory('error')}`;
+  result += `${formatCategory('error', pretty)}`;
   result += ' ';
-  result += styleText('gray', error.code);
+  result += color('gray', error.code, pretty);
   result += ': ';
   result += error.message;
   if (error.cause !== undefined) {
     result += '\n';
-    result += `[cause]: ${inspect(error.cause, { colors: shouldColorize() })}`.replace(/^/gmu, '  ');
+    result += `[cause]: ${inspect(error.cause, { colors: pretty })}`.replace(/^/gmu, '  ');
   }
   return result;
 }
 
-function formatLocation(fileName: string, start: DiagnosticPosition | undefined, cwd: string): string {
+function formatLocation(fileName: string, start: DiagnosticPosition | undefined, cwd: string, pretty: boolean): string {
   let result = '';
-  result += styleText('cyan', relative(cwd, fileName));
+  result += color('cyan', relative(cwd, fileName), pretty);
   if (start !== undefined) {
     result += ':';
-    result += styleText('yellow', start.line.toString());
+    result += color('yellow', start.line.toString(), pretty);
     result += ':';
-    result += styleText('yellow', start.column.toString());
+    result += color('yellow', start.column.toString(), pretty);
   }
   return result;
 }
 
-function formatCategory(category: DiagnosticCategory): string {
+function formatCategory(category: DiagnosticCategory, pretty: boolean): string {
   switch (category) {
     case 'error':
-      return styleText('red', 'error');
+      return color('red', 'error', pretty);
     case 'warning':
-      return styleText('yellow', 'warning');
+      return color('yellow', 'warning', pretty);
     default:
       throw new Error(`Unknown diagnostic category: ${String(category)}`);
   }
