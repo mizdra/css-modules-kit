f566af7bf6be1b9f2240e17a40f46d1220294334
mizdra <pp.mizdra@gmail.com>
2025-05-26 22:29:54 +0900

    refactor test codes

--- packages/ts-plugin/e2e/test/tsserver.ts
+++ packages/ts-plugin/e2e/test/tsserver.ts
@@ -28,7 +28,7 @@ interface Tsserver {
   sendCompletionInfo(
     args: server.protocol.CompletionsRequest['arguments'],
   ): Promise<server.protocol.CompletionInfoResponse>;
-  sendGetCodeFixes(args: server.protocol.CodeFixRequest['arguments']): Promise<server.protocol.GetCodeFixesResponse>;
+  sendGetCodeFixes(args: server.protocol.CodeFixRequest['arguments']): Promise<server.protocol.CodeFixResponse>;
 }
 
 export function launchTsserver(): Tsserver {
@@ -99,4 +99,103 @@ export function sortDefinitions(definitions: readonly ts.server.protocol.Definit
   return definitions.toSorted((a, b) => {
     return a.file.localeCompare(b.file) || a.start.line - b.start.line || a.start.offset - b.start.offset;
   });
+}
+
+type SimplifiedSpanGroup = {
+  file: string;
+  locs: ts.server.protocol.TextSpan[];
+};
+
+export function simplifySpanGroups(spanGroups: readonly ts.server.protocol.SpanGroup[]): SimplifiedSpanGroup[] {
+  return spanGroups.map((loc) => {
+    return {
+      file: formatPath(loc.file),
+      locs: loc.locs.map((loc) => ({
+        start: loc.start,
+        end: loc.end,
+        ...('prefixText' in loc ? { prefixText: loc.prefixText } : {}),
+        ...('suffixText' in loc ? { suffixText: loc.suffixText } : {}),
+      })),
+    };
+  });
+}
+
+export function sortSpanGroups(spanGroups: SimplifiedSpanGroup[]): SimplifiedSpanGroup[] {
+  const sortedLocs = spanGroups.toSorted((a, b) => {
+    return a.file.localeCompare(b.file);
+  });
+  for (const loc of sortedLocs) {
+    loc.locs.sort((a, b) => {
+      return a.start.line - b.start.line || a.start.offset - b.start.offset;
+    });
+  }
+  return sortedLocs;
+}
+
+type SimplifiedReferencesResponseItem = {
+  file: string;
+  start: ts.server.protocol.Location;
+  end: ts.server.protocol.Location;
+};
+
+export function simplifyRefItems(
+  refs: readonly ts.server.protocol.ReferencesResponseItem[],
+): SimplifiedReferencesResponseItem[] {
+  return refs.map((ref) => {
+    return {
+      file: formatPath(ref.file),
+      start: ref.start,
+      end: ref.end,
+    };
+  });
+}
+
+export function sortRefItems(refs: readonly SimplifiedReferencesResponseItem[]) {
+  return refs.toSorted((a, b) => {
+    return a.file.localeCompare(b.file) || a.start.line - b.start.line || a.start.offset - b.start.offset;
+  });
+}
+
+export function mergeSpanGroups(fileSpans: ts.server.protocol.FileSpan[]): SimplifiedSpanGroup[] {
+  const spanGroups: SimplifiedSpanGroup[] = [];
+  for (const fileSpan of fileSpans) {
+    const existingGroup = spanGroups.find((group) => group.file === fileSpan.file);
+    if (existingGroup) {
+      existingGroup.locs.push({ start: fileSpan.start, end: fileSpan.end });
+    } else {
+      spanGroups.push({
+        file: fileSpan.file,
+        locs: [{ start: fileSpan.start, end: fileSpan.end }],
+      });
+    }
+  }
+  return spanGroups;
+}
+
+type SimplifiedCompletionEntry = {
+  name: string;
+  sortText: string;
+  source?: string;
+  insertText?: string;
+};
+
+export function simplifyCompletionEntry(
+  entries: readonly ts.server.protocol.CompletionEntry[],
+): SimplifiedCompletionEntry[] {
+  return entries.map((entry) => {
+    return {
+      name: entry.name,
+      sortText: entry.sortText,
+      ...('source' in entry ? { source: entry.source } : {}),
+      ...('insertText' in entry ? { insertText: entry.insertText } : {}),
+    };
+  });
+}
+
+export function compareCompletionEntries(a: SimplifiedCompletionEntry, b: SimplifiedCompletionEntry) {
+  return (
+    a.sortText?.localeCompare(b.sortText ?? '') ||
+    a.source?.localeCompare(b.source ?? '') ||
+    a.name.localeCompare(b.name)
+  );
 }
