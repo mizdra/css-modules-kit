ea0658d9c4e9ea1dceb8049af9c948662122bcbb
mizdra <pp.mizdra@gmail.com>
2025-08-05 00:49:08 +0900

    refactor test

--- packages/core/src/checker.test.ts
+++ packages/core/src/checker.test.ts
@@ -1,28 +1,51 @@
 import { describe, expect, test } from 'vitest';
 import { checkCSSModule } from './checker.js';
+import { createExportBuilder } from './export-builder.js';
+import { resolve } from './path.js';
 import { createResolver } from './resolver.js';
 import { fakeCSSModule } from './test/css-module.js';
-import { fakeAtImportTokenImporter, fakeAtValueTokenImporter, fakeAtValueTokenImporterValue } from './test/token.js';
-import type { ExportBuilder } from './type.js';
+import {
+  fakeAtImportTokenImporter,
+  fakeAtValueTokenImporter,
+  fakeAtValueTokenImporterValue,
+  fakeToken,
+} from './test/token.js';
+import type { CSSModule } from './type.js';
 
 const resolver = createResolver({}, undefined);
 
+function prepareCheckerArgs<const T extends CSSModule[]>(cssModules: T) {
+  const getCSSModule = (path: string) => cssModules.find((m) => resolve(m.fileName) === resolve(path));
+  const matchesPattern = (path: string) => path.endsWith('.module.css');
+  const exportBuilder = createExportBuilder({
+    getCSSModule,
+    matchesPattern,
+    resolver,
+  });
+  return { cssModules, exportBuilder, matchesPattern, resolver, getCSSModule };
+}
+
 describe('checkCSSModule', () => {
   test('report diagnostics for non-existing module', () => {
-    const cssModule = fakeCSSModule({
-      fileName: '/a.module.css',
-      tokenImporters: [
-        fakeAtImportTokenImporter({ from: './b.module.css' }),
-        fakeAtValueTokenImporter({ from: './c.module.css', values: [fakeAtValueTokenImporterValue({ name: 'c_1' })] }),
-      ],
-    });
-    const exportBuilder: ExportBuilder = {
-      build: () => ({ allTokens: [] }),
-      clearCache: () => {},
-    };
-    const matchesPattern = () => true;
-    const getCSSModule = () => undefined;
-    const diagnostics = checkCSSModule(cssModule, exportBuilder, matchesPattern, resolver, getCSSModule);
+    const args = prepareCheckerArgs([
+      fakeCSSModule({
+        fileName: '/a.module.css',
+        tokenImporters: [
+          fakeAtImportTokenImporter({ from: './b.module.css' }),
+          fakeAtValueTokenImporter({
+            from: './c.module.css',
+            values: [fakeAtValueTokenImporterValue({ name: 'c_1' })],
+          }),
+        ],
+      }),
+    ]);
+    const diagnostics = checkCSSModule(
+      args.cssModules[0],
+      args.exportBuilder,
+      args.matchesPattern,
+      args.resolver,
+      args.getCSSModule,
+    );
     expect(diagnostics).toMatchInlineSnapshot(`
       [
         {
@@ -55,22 +78,28 @@ describe('checkCSSModule', () => {
     `);
   });
   test('report diagnostics for non-exported token', () => {
-    const cssModule = fakeCSSModule({
-      fileName: '/a.module.css',
-      tokenImporters: [
-        fakeAtValueTokenImporter({
-          from: './b.module.css',
-          values: [fakeAtValueTokenImporterValue({ name: 'b_1' }), fakeAtValueTokenImporterValue({ name: 'b_2' })],
-        }),
-      ],
-    });
-    const exportBuilder: ExportBuilder = {
-      build: () => ({ allTokens: ['b_1'] }),
-      clearCache: () => {},
-    };
-    const matchesPattern = () => true;
-    const getCSSModule = () => cssModule;
-    const diagnostics = checkCSSModule(cssModule, exportBuilder, matchesPattern, resolver, getCSSModule);
+    const args = prepareCheckerArgs([
+      fakeCSSModule({
+        fileName: '/a.module.css',
+        tokenImporters: [
+          fakeAtValueTokenImporter({
+            from: './b.module.css',
+            values: [fakeAtValueTokenImporterValue({ name: 'b_1' }), fakeAtValueTokenImporterValue({ name: 'b_2' })],
+          }),
+        ],
+      }),
+      fakeCSSModule({
+        fileName: '/b.module.css',
+        localTokens: [fakeToken({ name: 'b_1' })],
+      }),
+    ]);
+    const diagnostics = checkCSSModule(
+      args.cssModules[0],
+      args.exportBuilder,
+      args.matchesPattern,
+      args.resolver,
+      args.getCSSModule,
+    );
     expect(diagnostics).toMatchInlineSnapshot(`
       [
         {
@@ -90,35 +119,41 @@ describe('checkCSSModule', () => {
     `);
   });
   test('ignore token importers for unresolvable modules', () => {
-    const cssModule = fakeCSSModule({
-      fileName: '/a.module.css',
-      tokenImporters: [fakeAtImportTokenImporter({ from: './unresolvable.module.css' })],
-    });
-    const exportBuilder: ExportBuilder = {
-      build: () => ({ allTokens: [] }),
-      clearCache: () => {},
-    };
-    const matchesPattern = () => true;
-    const resolver = () => undefined;
-    const getCSSModule = () => undefined;
-    const diagnostics = checkCSSModule(cssModule, exportBuilder, matchesPattern, resolver, getCSSModule);
+    const args = prepareCheckerArgs([
+      fakeCSSModule({
+        fileName: '/a.module.css',
+        tokenImporters: [fakeAtImportTokenImporter({ from: './unresolvable.module.css' })],
+      }),
+    ]);
+    const diagnostics = checkCSSModule(
+      args.cssModules[0],
+      args.exportBuilder,
+      args.matchesPattern,
+      () => undefined, // Simulate unresolvable module
+      args.getCSSModule,
+    );
     expect(diagnostics).toEqual([]);
   });
   test('ignore token importers that do not match the pattern', () => {
-    const cssModule = fakeCSSModule({
-      fileName: '/a.module.css',
-      tokenImporters: [
-        fakeAtImportTokenImporter({ from: './b.module.css' }),
-        fakeAtValueTokenImporter({ from: './c.module.css', values: [fakeAtValueTokenImporterValue({ name: 'c_1' })] }),
-      ],
-    });
-    const exportBuilder: ExportBuilder = {
-      build: () => ({ allTokens: [] }),
-      clearCache: () => {},
-    };
-    const matchesPattern = () => false;
-    const getCSSModule = () => undefined;
-    const diagnostics = checkCSSModule(cssModule, exportBuilder, matchesPattern, resolver, getCSSModule);
+    const args = prepareCheckerArgs([
+      fakeCSSModule({
+        fileName: '/a.module.css',
+        tokenImporters: [
+          fakeAtImportTokenImporter({ from: './b.module.css' }),
+          fakeAtValueTokenImporter({
+            from: './c.module.css',
+            values: [fakeAtValueTokenImporterValue({ name: 'c_1' })],
+          }),
+        ],
+      }),
+    ]);
+    const diagnostics = checkCSSModule(
+      args.cssModules[0],
+      args.exportBuilder,
+      (path: string) => path === '/a.module.css', // Only match the current module
+      args.resolver,
+      args.getCSSModule,
+    );
     expect(diagnostics).toEqual([]);
   });
 });
