10e2057df1807843db62865774b95207fe4f5af7
mizdra <pp.mizdra@gmail.com>
2025-08-09 13:48:07 +0900

    workspace is deprecated so use `projects`

---
+++ vitest.config.ts
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-restricted-imports
+import { resolve } from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        test: {
+          name: 'unit',
+          include: ['packages/*/src/**/*.test.ts'],
+        },
+        resolve: {
+          alias: {
+            '@css-modules-kit/core': resolve('packages/core/src/index.ts'),
+          },
+        },
+      },
+      {
+        test: {
+          name: 'e2e',
+          include: ['packages/*/e2e/**/*.test.ts'],
+          exclude: ['packages/vscode/e2e/**'],
+        },
+      },
+    ],
+  },
+});
