180d3451d5ba7e6ce22dfb172bba22d31ea0b6cd
mizdra <pp.mizdra@gmail.com>
2025-08-31 00:54:26 +0900

    chore: fix test config

--- scripts/vitest-e2e-test-setup.ts
+++ scripts/vitest-e2e-test-setup.ts
@@ -2,8 +2,8 @@ import { execSync } from 'node:child_process';
 import type { TestProject } from 'vitest/node';
 
 export default function setup(project: TestProject) {
-  execSync('npm run build', { stdio: 'inherit' });
+  execSync('npm run build -- --noCheck', { stdio: 'inherit' });
   project.onTestsRerun(() => {
-    execSync('npm run build', { stdio: 'inherit' });
+    execSync('npm run build -- --noCheck', { stdio: 'inherit' });
   });
 }
