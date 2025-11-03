412f33e25ed5a1d6245d17c5cb87229a37f55820
mizdra <pp.mizdra@gmail.com>
2025-11-03 17:40:23 +0900

    Log internal errors with logger (#275)
    * feat: log internal errors with logger
    
    * add changelog

---
+++ .changeset/busy-snails-play.md
@@ -0,0 +1,5 @@
+---
+'@css-modules-kit/codegen': patch
+---
+
+feat: log internal errors with logger
--- packages/codegen/src/test/logger.ts
+++ packages/codegen/src/test/logger.ts
@@ -4,7 +4,7 @@ import type { Logger } from '../logger/logger.js';
 export function createLoggerSpy() {
   return {
     logDiagnostics: vi.fn(),
-    logSystemError: vi.fn(),
+    logError: vi.fn(),
     logMessage: vi.fn(),
   } satisfies Logger;
 }
