dbbfa7b46b7cc718584ef1c6de6f0b8cd53331dd
mizdra <pp.mizdra@gmail.com>
2025-12-21 18:36:26 +0900

    chore: fix version script

--- scripts/version.sh
+++ scripts/version.sh
@@ -8,8 +8,13 @@ npx changeset version
 
 # Sync zed extension version with the `version` field in crates/zed/package.json
 v=$(jq -r .version crates/zed/package.json)
-sed -i '' -E "s/^version = \".*\"/version = \"$v\"/" crates/zed/extension.toml
-sed -i '' -E "s/^version = \".*\"/version = \"$v\"/" crates/zed/Cargo.toml
+if [[ "$(uname)" == "Darwin" ]]; then
+  sed -i '' -E "s/^version = \".*\"/version = \"$v\"/" crates/zed/extension.toml
+  sed -i '' -E "s/^version = \".*\"/version = \"$v\"/" crates/zed/Cargo.toml
+else
+  sed -i -E "s/^version = \".*\"/version = \"$v\"/" crates/zed/extension.toml
+  sed -i -E "s/^version = \".*\"/version = \"$v\"/" crates/zed/Cargo.toml
+fi
 
 # Update package-lock.json
 npm i
