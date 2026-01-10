ab013611e31e1a69cce6ae4f50ba3430c881f93f
mizdra <pp.mizdra@gmail.com>
2026-01-10 19:36:12 +0900

    fix(core): prevent URL specifiers from being resolved by import aliases

--- packages/core/src/resolver.ts
+++ packages/core/src/resolver.ts
@@ -1,13 +1,14 @@
 import type { CompilerOptions } from 'typescript';
 import ts from 'typescript';
-import { resolve } from './path.js';
 import type { Resolver, ResolverOptions } from './type.js';
+import { isURLSpecifier } from './util.js';
 
 export function createResolver(
   compilerOptions: CompilerOptions,
   moduleResolutionCache: ts.ModuleResolutionCache | undefined,
 ): Resolver {
   return (specifier: string, options: ResolverOptions) => {
+    if (isURLSpecifier(specifier)) return undefined;
     const host: ts.ModuleResolutionHost = {
       ...ts.sys,
       fileExists: (fileName) => {
@@ -26,7 +27,7 @@ export function createResolver(
     );
     if (resolvedModule) {
       // TODO: Logging that the paths is used.
-      return resolve(resolvedModule.resolvedFileName.replace(/\.module\.d\.css\.ts$/u, '.module.css'));
+      return resolvedModule.resolvedFileName.replace(/\.module\.d\.css\.ts$/u, '.module.css');
     }
     return undefined;
   };
