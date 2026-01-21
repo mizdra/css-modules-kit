b81f8ad9041533e646eccaa2b2dedf1ba8be99f6
apple-yagi <applesryu@gmail.com>
2026-01-21 16:57:52 +0900

    chore: update the dts for examples

--- examples/3-import-alias/generated/src/a.module.css.d.ts
+++ examples/3-import-alias/generated/src/a.module.css.d.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck
+function blockErrorType<T>(val: T): [0] extends [(1 & T)] ? {} : T;
 declare const styles = {
-  ...(await import('@/src/b.module.css')).default,
-  ...(await import('#src/b.module.css')).default,
+  ...blockErrorType((await import('@/src/b.module.css')).default),
+  ...blockErrorType((await import('#src/b.module.css')).default),
 };
 export default styles;
