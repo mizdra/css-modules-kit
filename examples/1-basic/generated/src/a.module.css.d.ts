44c1dbf7b248d0795bd7b5e3b93abfb116f459a7
Ryuya Yanagi <57742720+apple-yagi@users.noreply.github.com>
2026-01-21 18:34:23 +0900

    chore: update the dts for examples (#308)

--- examples/1-basic/generated/src/a.module.css.d.ts
+++ examples/1-basic/generated/src/a.module.css.d.ts
@@ -1,11 +1,12 @@
 // @ts-nocheck
+function blockErrorType<T>(val: T): [0] extends [(1 & T)] ? {} : T;
 declare const styles = {
   a_1: '' as readonly string,
   a_2: '' as readonly string,
   a_2: '' as readonly string,
   a_3: '' as readonly string,
   a_4: '' as readonly string,
-  ...(await import('./b.module.css')).default,
+  ...blockErrorType((await import('./b.module.css')).default),
   c_1: (await import('./c.module.css')).default.c_1,
   c_alias: (await import('./c.module.css')).default.c_2,
 };
