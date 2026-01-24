073c3640ba131cabc01dcd74daee037880383ce7
mizdra <pp.mizdra@gmail.com>
2026-01-24 15:41:52 +0900

    fix(core): fix the issue where `*.css` import resolution fails

--- packages/core/src/resolver.ts
+++ packages/core/src/resolver.ts
@@ -12,8 +12,8 @@ export function createResolver(
     const host: ts.ModuleResolutionHost = {
       ...ts.sys,
       fileExists: (fileName) => {
-        if (fileName.endsWith('.module.d.css.ts')) {
-          return ts.sys.fileExists(fileName.replace(/\.module\.d\.css\.ts$/u, '.module.css'));
+        if (fileName.endsWith('.d.css.ts')) {
+          return ts.sys.fileExists(fileName.replace(/\.d\.css\.ts$/u, '.css'));
         }
         return ts.sys.fileExists(fileName);
       },
@@ -27,7 +27,7 @@ export function createResolver(
     );
     if (resolvedModule) {
       // TODO: Logging that the paths is used.
-      return resolvedModule.resolvedFileName.replace(/\.module\.d\.css\.ts$/u, '.module.css');
+      return resolvedModule.resolvedFileName.replace(/\.d\.css\.ts$/u, '.css');
     }
     return undefined;
   };
