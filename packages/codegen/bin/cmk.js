a73efbc24d19ed81c7874adce335567ffd2d7709
mizdra <pp.mizdra@gmail.com>
2026-02-20 00:36:35 +0900

    chore: fix lint errors

--- packages/codegen/bin/cmk.js
+++ packages/codegen/bin/cmk.js
@@ -31,7 +31,8 @@ try {
   // - Watch mode: Outputs errors to the terminal but does not terminate the process. Continues watching the file.
   if (args.watch) {
     const watcher = await runCMKInWatchMode(args, logger);
-    process.on('SIGINT',  async () => watcher.close());
+    // oxlint-disable-next-line typescript/no-misused-promises
+    process.on('SIGINT', async () => watcher.close());
   } else {
     const success = await runCMK(args, logger);
     if (!success) {
